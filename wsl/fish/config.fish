68197e1d0884ec60a5b7340a1042e9c3e5e9c64f
abdullahkhalid00 <iabdulahk@gmail.com>
2026-01-09 19:08:02 +0500

    feat(shell): add fish shell config

---
+++ wsl/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Remove fish greeting at shell startup
+    set fish_greeting ''
+
+    # Run fastfetch on shell startup (disabled for now)
+    # fastfetch
+end
